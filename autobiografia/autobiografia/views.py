f421ce70c28bfc1375fe9b16337553343b0d0146
edunickdev <edunsarmiento@soy.sena.edu.co>
2023-01-25 16:08:46 -0500

    otro commit

--- autobiografia/autobiografia/views.py
+++ autobiografia/autobiografia/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-	return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
 
 
 def gallery(request):
-	imagen = {'imagen 1', 'imagen 2', 'imagen 3', 'imagen 4', 'imagen 5', 'imagen 6', 'imagen 7', 'imagen 8', 'imagen 9', 'imagen 10', 'imagen 11', 'imagen 12'}
-	context = {'imagen': imagen}
-	return render(request, 'gallery.html', context)
+    imagen = {'imagen 1', 'imagen 2', 'imagen 3', 'imagen 4', 'imagen 5'}
+    context = {'imagen': imagen}
+    return render(request, 'gallery.html', context)
 
 
 def pruebas(request):
-	return render(request, 'layouts/base.html', {})
+    return render(request, 'layouts/base.html', {})
